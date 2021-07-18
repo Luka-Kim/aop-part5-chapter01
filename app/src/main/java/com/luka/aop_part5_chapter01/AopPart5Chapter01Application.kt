812d76b10b1d5aa15a576ce3de9b3085f2cba1a2
LUKA KIM <luka.kim@sothecode.com>
2021-07-19 00:21:19 +0900

    Initial commit

---
+++ app/src/main/java/com/luka/aop_part5_chapter01/AopPart5Chapter01Application.kt
@@ -0,0 +1,20 @@
+package com.luka.aop_part5_chapter01
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class AopPart5Chapter01Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // TODO koin trigger
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@AopPart5Chapter01Application)
+            modules()
+        }
+    }
+}
